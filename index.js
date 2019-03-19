0d98b3cdb0b39d6ea1914b85298f8059214e5b51
ashleighyslop <ahyslop2@sapient.com>
2019-03-19 16:47:42 +0000

    semantic html app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = require('express')();
+const path = require('path');
+
+app.get('/',function(req,res) {
+    res.sendFile(path.resolve(__dirname, './formatted.html'));
+  });
+app.use(express.static('public'))
+// app.get("/image", function(request, response) {
+//     response.render("image");
+//    });
+  
+app.listen(3000);
+console.log('app is listening at port 3000');
