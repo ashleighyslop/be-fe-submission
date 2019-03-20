9ea75a4958d1c54637a6bb29f3c023b9cc2bb132
ashleighyslop <ahyslop2@sapient.com>
2019-03-20 14:34:47 +0000

    set up with express to run

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const path = require('path');
 app.get('/',function(req,res) {
     res.sendFile(path.resolve(__dirname, './formatted.html'));
   });
-app.use(express.static('public'))
-// app.get("/image", function(request, response) {
-//     response.render("image");
-//    });
+app.use(express.static('public'));
   
 app.listen(3000);
 console.log('app is listening at port 3000');
